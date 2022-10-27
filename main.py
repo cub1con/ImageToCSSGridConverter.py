85189f16c2c9f7d45404635d5d170f76f6a284d2
cub1con <18596772+cub1con@users.noreply.github.com>
2022-10-27 03:01:15 +0200

    Added a tutorial, numpy, logging and some performance stats

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 #hello <3
 #HI !!!
+# Tutorial:
+# - Download the folder
+# - Change imgName in row 61 to the filename
+# - Open command line in folder (shift + mouse right in folder)
+# - Run 'python3 main.py'
 
 import multiprocessing
 from multiprocessing.pool import ThreadPool
 from PIL import Image
+import numpy as np
+import time
 
 def createImageFile(image, imgName):
   name = "template.html"
@@ -22,6 +29,8 @@ def createStyleFile(size):
   file = file.replace("%REPLACEHEIGHT%", str(size[1]))
   writeTemplate(name, file)
 
+def getTimeDifference(time1, time2):
+  return f"{time2 - time1:0.4f} Seconds"
 
 def fillTemplate(template, key, content):
   return template.replace(key, content)
@@ -36,26 +45,32 @@ def writeTemplate(template, content):
 
 def getDivForPixels(pixels, fromH, toH, width, threadId):
   print(f"Creating thread {threadId} from {fromH} to {toH}")
+  startTime = time.perf_counter()
   str = ""
   for h in range(fromH, toH):
     print(f"T{threadId}: {h}", flush=True)
     for w in range(width):
       #print(f"h: {h} | w: {w}")
       #r, g, b, a = img.getpixel((w, h))      
-      r, g, b, a = pixels[w, h]
+      r, g, b, a = pixels[h, w]
       if a == 0:
-        print(f"Skipped {w, h}")  
+        #print(f"Skipped {h, w}")  
         continue
 
       str = str + (
         f"<div style=\"background-color:rgba({r},{g},{b},{a});grid-column:{w+1}/{w+2};grid-row:{h+1}/{h+2}\"></div>\n"
       )
+  
+  print(f"Thread {threadId} finished {(toH - fromH) * width} pixel in {getTimeDifference(startTime, time.perf_counter())} ")
   return str
 
-imgName = "magala_small.png"
-img = Image.open(imgName)  # Can be many different formats.
-pixels = img.load()
-print(img.format, img.size, img.mode)  # Get the format, size and color mode
+
+startTime = time.perf_counter()
+imgName = "magala_small.png" # Filename
+img = Image.open(imgName)  # Can be many different formats. Currently we support only RGBA images!
+pixels = np.asarray(img) # Convert the image data to an numpy array
+#pixels = np.array(img).reshape((img.height, img.width, 4)).astype(np.uint8)
+print(imgName, img.format, img.size, img.mode)  # Print the name, format, size and color mode
 
 cores = multiprocessing.cpu_count() #//2
 rowsPerCore = img.height // cores
@@ -63,6 +78,8 @@ distributed = rowsPerCore * cores
 loss = img.height - distributed
 
 pool = ThreadPool(processes=cores)
+
+print(f"Distributing workload on {cores} threads a {rowsPerCore} rows per core")
 
 tasks = []
 for c in range(cores):
@@ -75,5 +92,11 @@ return_val = ""
 for t in tasks:
   return_val += t.get()
 
+print("Writing stylesheet")
 createStyleFile(img.size)
+
+print("Writing html file")
 createImageFile(return_val, imgName)
+
+print("Finished!")
+print(f"Programm finished {img.height * img.width} pixel in {getTimeDifference(startTime, time.perf_counter())}")
